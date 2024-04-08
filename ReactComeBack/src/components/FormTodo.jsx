e7e91f8a5e33e5053d9ea943e7d6e999c8a213fb
SrnDev16 <saranjangsuk@gmail.com>
2024-04-08 17:51:20 +0700

    little bit

--- ReactComeBack/src/components/FormTodo.jsx
+++ ReactComeBack/src/components/FormTodo.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
 const FormTodo = () => {
   const [title, setTitle] = useState("");
   const [list, setList] = useState([]);
 
-
   return (
-    <div>FormTodo</div>
-  )
-}
+    <div className="bg-orange-600">
+      <h1 className="text-indigo-500">hello</h1>
+    </div>
+  );
+};
 
-export default FormTodo
+export default FormTodo;
