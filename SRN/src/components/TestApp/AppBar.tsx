0c441f5de3534c0bc0aefe8be1fc5e1fd243f38e
SrnDev16 <saranjangsuk@gmail.com>
2024-07-06 16:25:29 +0700

    appbar
    responsive version 1

--- SRN/src/components/TestApp/AppBar.tsx
+++ SRN/src/components/TestApp/AppBar.tsx
@@ -1,32 +1,62 @@
 import { useState } from "react";
 import { CgMenu } from "react-icons/cg";
+import { FaRegWindowClose } from "react-icons/fa";
 
 const AppBar = () => {
   const [menu, setMenu] = useState<boolean>(true);
 
   const toggleMenu = () => setMenu(!menu);
   return (
-    <div>
-      <div className="relative w-full h-10 bg-blue-300 px-3 flex items-center justify-between">
-        <div>Logo</div>
-        <div>
-          <button onClick={toggleMenu}>
-            <CgMenu />
-          </button>
+      <div>
+        <div className="relative w-full h-10 bg-blue-300 px-3 flex items-center justify-between">
+          <div>Logo</div>
+          <div>
+            <button onClick={toggleMenu} className="md:hidden">
+             {menu ? <CgMenu /> : <FaRegWindowClose />}
+            </button>
+            <div className="hidden md:block">
+              <ul className="flex">
+                <li className="mr-5">
+                  <a href="#">About</a>
+                </li>
+                <li className="mr-5">
+                  <a href="#">About</a>
+                </li>
+                <li className="mr-5">
+                  <a href="#">About</a>
+                </li>
+                <li className="mr-5">
+                  <a href="#">About</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
+        <button className="ease-in duration-300 bg-red-500 hover:translate-x-2">
+          test
+        </button>
+        <div
+          className={`${
+            menu ? "hidden -z-10" : "block absolute top-10 z-10"
+          } h-60 w-full bg-blue-300`}
+        >
+          <ul className="flex flex-col justify-evenly items-center h-full">
+            <li className="mr-5">
+              <a href="#" onClick={toggleMenu}>About</a>
+            </li>
+            <li className="mr-5">
+              <a href="#" onClick={toggleMenu}>About</a>
+            </li>
+            <li className="mr-5">
+              <a href="#" onClick={toggleMenu}>About</a>
+            </li>
+            <li className="mr-5">
+              <a href="#" onClick={toggleMenu}>About</a>
+            </li>
+          </ul>
+        </div>
+        <div>test1</div>
       </div>
-      <button className="ease-in duration-300 bg-red-500 hover:translate-x-2">
-        test
-      </button>
-      <div
-        className={`${
-          menu ? "hidden -z-10" : "block absolute top-10 z-10"
-        } h-60 w-full bg-blue-300`}
-      >
-        box
-      </div>
-      <div>test1</div>
-    </div>
   );
 };
 
